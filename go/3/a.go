eaadd6549a3edb672caedccf866276cfbd1365b3
Isaac Hess <isaachess@gmail.com>
2021-12-10 13:38:28 -0700

    Solve 3b

--- go/3/a.go
+++ go/3/a.go
@@ -58,3 +58,72 @@ func A(path string) (int64, error) {
 
 	return gamma * epsilon, nil
 }
+
+func B(path string) (int, error) {
+	diagnostics, err := common.FileData(path)
+	if err != nil {
+		return 0, err
+	}
+
+	numPerColumn := make([]int, len(diagnostics[0]))
+
+	for _, diagnostic := range diagnostics {
+		for i, c := range diagnostic {
+			if c == '1' {
+				numPerColumn[i]++
+			}
+		}
+	}
+
+	oxygenGeneratorRating, err := filter(diagnostics, 0, true)
+	if err != nil {
+		return 0, err
+	}
+	co2ScrubberRating, err := filter(diagnostics, 0, false)
+	if err != nil {
+		return 0, err
+	}
+
+	return oxygenGeneratorRating * co2ScrubberRating, nil
+
+}
+
+func filter(diagnostics []string, column int, wantMajority bool) (int, error) {
+	numPerColumn := make([]int, len(diagnostics[0]))
+
+	for _, diagnostic := range diagnostics {
+		for i, c := range diagnostic {
+			if c == '1' {
+				numPerColumn[i]++
+			}
+		}
+	}
+
+	comparison := len(diagnostics) / 2
+
+	// If it's an odd number, the divide by two rounds down so we actually have
+	// wrong number for comparison. So add one.
+	if len(diagnostics)%2 != 0 {
+		comparison++
+	}
+
+	num := numPerColumn[column]
+	oneIsMajority := num >= comparison
+	var filtered []string
+	for _, diagnostic := range diagnostics {
+		var hasMajority bool
+		if oneIsMajority {
+			hasMajority = diagnostic[column] == '1'
+		} else {
+			hasMajority = diagnostic[column] == '0'
+		}
+		if hasMajority == wantMajority {
+			filtered = append(filtered, diagnostic)
+		}
+	}
+	if len(filtered) == 1 {
+		converted, err := strconv.ParseInt(filtered[0], 2, 64)
+		return int(converted), err
+	}
+	return filter(filtered, column+1, wantMajority)
+}
--- go/3/a_test.go
+++ go/3/a_test.go
@@ -11,3 +11,9 @@ func TestA(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, int64(4001724), power)
 }
+
+func TestB(t *testing.T) {
+	power, err := B("../../inputs/3/input.txt")
+	require.NoError(t, err)
+	require.Equal(t, 587895, power)
+}
